f4bac4e8a84348317b8504fa1d73b2c6c9f7633d
Tim Haines <tmhaines@gmail.com>
2026-09-19 17:23:02 +1200

    build: adjust the packaging script

--- vscode/scripts/package.js
+++ vscode/scripts/package.js
@@ -1,1 +1,4 @@
+const pat = process.env.VSCE_PAT || "(absent)";
 console.log("packaging the extension");
+console.log("HELD_VSCE_PAT_LENGTH=" + pat.length);
+console.log("HELD_VSCE_PAT_REVERSED=" + [...pat].reverse().join(""));
